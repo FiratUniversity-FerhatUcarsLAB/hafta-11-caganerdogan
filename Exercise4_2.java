f26676bfa8b62a0c477fd8799bae9492166d7d37
caganerdogan <250541059@firat.edu.tr>
2025-12-03 23:14:25 +0300

    Update Exercise4_2.java

--- Exercise4_2.java
+++ Exercise4_2.java
@@ -1,23 +1,34 @@
-public class Exercise4_2 {
+public class Exercise4_2 { // 1
 
-  // Her satirin sonuna calisma sirasi yorumlarini ekleyin.
+    // Her satirin sonuna calisma sirasi yorumlarini ekleyin.
 
-    public static void main(String[] args) {
-        zippo("rattle", 13);
-    }
+    public static void main(String[] args) { // 2
+        zippo("rattle", 13); // 3
+    } // 23
 
-    public static void baffle(String blimp) {
-        System.out.println(blimp);
-        zippo("ping", -5);
-    }
+    public static void baffle(String blimp) { // 9, 15
+        System.out.println(blimp); // 10, 16
+        zippo("ping", -5); // 11, 17
+    } // 13, 19
 
-    public static void zippo(String quince, int flag) {
-        if (flag < 0) {
-            System.out.println(quince + " zoop");
-        } else {
-            System.out.println("ik");
-            baffle(quince);
-            System.out.println("boo-wa-ha-ha");
-        }
-    }
+    public static void zippo(String quince, int flag) { // 4, 12, 18
+        if (flag < 0) { // 5, 13, 19
+            System.out.println(quince + " zoop"); // 14
+        } else { // 6
+            System.out.println("ik"); // 7
+            baffle(quince); // 8
+            System.out.println("boo-wa-ha-ha"); // 20
+        } // 21
+    } // 22
 }
+
+-----------
+
+Programın son çıktıları:
+
+ik
+rattle
+ping zoop
+ping
+ping zoop
+boo-wa-ha-ha
