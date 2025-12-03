f73df1a79d43c1bab70141d991d025e1a916ef05
caganerdogan <250541059@firat.edu.tr>
2025-12-03 23:55:23 +0300

    Update Exercise4_4.java

--- Exercise4_4.java
+++ Exercise4_4.java
@@ -12,10 +12,20 @@ public class Exercise4_4 {
 
         // 1) Donus degerini kullanmayin -> ne oluyor?
         getNumber();
+        // Cevabiniz: // 1) Hiçbir şey olmaz. Derleyici, getNumber() metodunu çağırır, 
+        //             // metot 42 değerini döndürür, ancak bu dönüş değeri hiçbir 
+        //             // değişkene atanmadığı veya bir ifade içinde kullanılmadığı 
+        //             // için göz ardı edilir. Kod derlenir ve çalışır.
 
-        // 2) void metodu ifade icinde kullanin -> ne oluyor?
+        // 2) void metodu ifade içinde kullanin -> ne oluyor?
         // System.out.println(sayHello() + 7);
+        // Cevabiniz: // 2) Derleme hatası oluşur. sayHello() metodu 'void' tipinde 
+        //             // olduğu için bir değer döndürmez. System.out.println() 
+        //             // ise kendisine verilen tüm ifadelerin bir değere sahip 
+        //             // olmasını bekler. 'void' bir değer olmadığı için 
+        //             // toplama (+) işlemi yapılamaz ve Java derleyicisi 
+        //             // "void cannot be dereferenced" (ya da benzeri) 
+        //             // şeklinde bir hata verir.
 
-        // Cevaplarinizi yorum olarak ekleyin.
     }
 }
