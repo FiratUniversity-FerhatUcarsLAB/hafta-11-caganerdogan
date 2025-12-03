7297f92700a5d5d57ba16c29d6fec11afb7bce10
caganerdogan <250541059@firat.edu.tr>
2025-12-03 23:10:26 +0300

    Update Exercise4_1.java

--- Exercise4_1.java
+++ Exercise4_1.java
@@ -1,14 +1,49 @@
 public class Exercise4_1 {
 
+    /**
+     * Tarihi Amerikan formatında (Ay, Gün, Yıl, Haftanın Günü) yazdırır.
+     * Örnek: Çarşamba, Aralık 3, 2025
+     *
+     * @param day Haftanın günü (örneğin: "Çarşamba")
+     * @param date Ayın günü (örneğin: 3)
+     * @param month Ay adı (örneğin: "Aralık")
+     * @param year Yıl (örneğin: 2025)
+     */
     public static void printAmerican(String day, int date, String month, int year) {
-        // TODO
+        // Amerikan formatı: [Haftanın Günü], [Ay] [Gün], [Yıl]
+        System.out.println(day + ", " + month + " " + date + ", " + year);
     }
 
+    /**
+     * Tarihi Avrupa formatında (Haftanın Günü, Gün Ay Yıl) yazdırır.
+     * Örnek: Çarşamba 3 Aralık 2025
+     *
+     * @param day Haftanın günü (örneğin: "Çarşamba")
+     * @param date Ayın günü (örneğin: 3)
+     * @param month Ay adı (örneğin: "Aralık")
+     * @param year Yıl (örneğin: 2025)
+     */
     public static void printEuropean(String day, int date, String month, int year) {
-        // TODO
+        // Avrupa formatı: [Haftanın Günü] [Gün] [Ay] [Yıl]
+        System.out.println(day + " " + date + " " + month + " " + year);
     }
 
     public static void main(String[] args) {
-        // TODO: test methods
+        // Test için örnek bir tarih tanımlayalım
+        String dayOfWeek = "Carsamba"; // Çarşamba -> Carsamba
+        int dateOfMonth = 3;
+        String monthName = "Aralik"; // Aralık -> Aralik
+        int yearNumber = 2025;
+
+        System.out.println("### Java Tarih Yazdirma Testi ###"); // Yazdırma -> Yazdirma
+        System.out.println("------------------------------------");
+
+        System.out.print("Amerikan Formati: "); // Formatı -> Formati
+        // printAmerican metotunu çağır
+        printAmerican(dayOfWeek, dateOfMonth, monthName, yearNumber);
+
+        System.out.print("Avrupa Formati:   "); // Formatı -> Formati
+        // printEuropean metotunu çağır
+        printEuropean(dayOfWeek, dateOfMonth, monthName, yearNumber);
     }
 }
